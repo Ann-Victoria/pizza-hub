5c2537a5dce8de178cc30f0cf62f32bc15dd057f
Victoria <wwaruri@gmail.com>
2021-08-03 06:50:48 +0300

    add delivery cost in summary

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,12 @@
-function order(size,crust,topping,total) {
+function order(size,crust,topping,delivery,cost,total) {
   this.size = size;
   this.crust = crust;
   this.topping = topping;
-  this.total = parseInt(size) + parseInt(crust) + parseInt(topping)
+  this.delivery = delivery;
+  this.cost = parseInt(size) + parseInt(crust) + parseInt(topping);
+  this.total = parseInt(size) + parseInt(crust) + parseInt(topping) + parseInt(delivery);
+  
+  
 }
 function myButton(event) {
   event.preventDefault()
@@ -17,16 +21,27 @@ function myButton(event) {
   $("ul#order").append("<li>Size Amount: <span class='order'>" + pizzaOrder.size + "</span></li>");
   $("ul#order").append("<li>Crust Amount: <span class='order'>" + pizzaOrder.crust + "</span></li>");
   $("ul#order").append("<li>Topping Amount: <span class='order'>" + pizzaOrder.topping + "</span></li>");
-  $("ul#order").append("<li>Total Amount: <span class='order'>"  + amount.total + "</span></li>");
+  $("ul#order").append("<li>Pizza Cost: <span class='order'>"  + amount.cost + "</span></li>");
 
-  var delivery = document.querySelector('input[name = delivery]:checked').value
-  if(delivery==='200') {
+  var pizzaDelivery = document.querySelector('input[name = delivery]:checked').value
+  if(pizzaDelivery==='200') {
     prompt('Enter location')
     alert('Your order will be delivered after check-out')
   }
+  
 }
 
+function checkOut() {
+  var pizzaSize = document.querySelector('input[name = size]:checked').value
+  var pizzaTopping = document.querySelector('input[name =topping]:checked').value
+  var pizzaCrust = document.getElementById('crust').value
+  var pizzaDelivery = document.querySelector('input[name = delivery]:checked').value
 
+  var totalAmount = new order(pizzaSize, pizzaTopping, pizzaCrust,pizzaDelivery)
+
+  $("ol#summary").append("<li>Delivery Amount: <span class='summary'>" + totalAmount.delivery + "</span></li>");
+}
+  
 
    
     
